2db4fdf883d5d2649c69d2f616f881443dea64ac
Ulukagan <139387427+Ulukagan@users.noreply.github.com>
2024-05-31 23:43:39 +0200

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -17,23 +17,16 @@ styled_caption = """
     Insurance is our Job
 </p>
 """
-# HTML/CSS to center the image
-centered_image_html = f"""
-<div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
-    <img src="data:image/jpeg;base64,{img_str}" width="600" style="margin-bottom: 20px;">
-</div>
-"""
 image = Image.open('main_image_insurance.jpeg')
-st.image(centered_image_html, width=600)
+st.image(image, width=600)
 st.markdown(styled_caption, unsafe_allow_html=True)
 #st.markdown("<h1 style='text-align: center; font-family: Arial, sans-serif; font-size: 20px; color: blue;font-weight: bold;'main_image_insurance.jpeg", unsafe_allow_html=True)
-
 
 st.markdown("**Please fill the below form :**")
 with st.form(key="Form :", clear_on_submit = True):
     Name = st.text_input("Name : ")
     Email = st.text_input("Email ID : ")
-    File = st.file_uploader(label = "Upload file", type=["pdf","docx"])
+    File = st.file_uploader(label = "Upload file", type=["pdf", "png", "jpg"])
     Submit = st.form_submit_button(label='Submit')
     
 
