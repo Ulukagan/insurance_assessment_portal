471b568b66e46fe9daa9c7768be612d81f777606
Ulukagan <139387427+Ulukagan@users.noreply.github.com>
2024-06-03 13:28:38 +0200

    Update streamlit_main2.py

--- streamlit_main2.py
+++ streamlit_main2.py
@@ -13,7 +13,7 @@ st.set_page_config(
 # Custom styled caption using HTML
 styled_caption = """
 <p style="text-align: center; font-family: Arial, sans-serif; font-size: 14px; color: blue; font-weight: bold;">
-    Insurance is our Job
+    Your Best Insurance AI-Partner...
 </p>
 """
 
